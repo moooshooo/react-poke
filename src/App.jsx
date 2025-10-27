00474d8ed5e7dfd906756eaa1dd0272a0716d595
moshito <mosho@me.com>
2025-10-27 20:08:01 +0100

    Played with promise.all that creates a good asynkon fetching for level 2 and 3 of the APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
 import ProgressBar from "./ProgressBar";
-console.log("Powered by: Mos\n===============")
+
+console.log("Powered by: Mos\n===============");
 function App() {
-  const [howMany, setHowMany] = useState(7);
+  const [howMany, setHowMany] = useState(3);
   const API_URL = `https://pokeapi.co/api/v2/pokemon?limit=${howMany}&offset=0`;
   const [pokemons, setPokemons] = useState(0);
-  const [pokemonName, setPokemonName] = useState([])
+  const [pokemonInfo, setPokemonInfo] = useState([]);
+  const [aboutPoke, setAboutPoke] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [fetchData, setFetchData] = useState(false);
-
-  const changeHowMany = (e) => {
-    setHowMany(e.target.value);
-  };
-  console.log(`Will load ${howMany} pokemons`)
+  const [pokemonData, setPokemonData] = useState([]);
+  const [speciesData, setSpeciesData] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
@@ -22,23 +21,62 @@ function App() {
       const getTheData = await fetch(API_URL);
       const parsedData = await getTheData.json();
       setPokemons(parsedData.results.length);
-      setPokemonName(parsedData.results)
-      console.log(typeof pokemonName)
+      setPokemonInfo(parsedData.results);
+      //console.log(typeof pokemonName);
+
+      console.log("Retriving more info som second API");
+
+      const detailedData = await Promise.all(
+        parsedData.results.map(async (poke) => {
+          //Detaljer
+          const pokeRes = await fetch(poke.url);
+          const pokeData = await pokeRes.json();
+
+          //species
+          const speciesRes = await fetch(pokeData.species.url);
+          const speciesData = await speciesRes.json();
+          return {pokeData,speciesData}
+        })
+      );
+      setPokemonData(detailedData.map(d => d.pokeData));
+      setSpeciesData(detailedData.map(d => d.speciesData));
+      setAboutPoke(detailedData);
+
+      console.log("Now you have more info");
     }
     getPokemons();
+
+    async function getMorePokeInfo() {
+      console.log("Now you have more info");
+    }
+    getMorePokeInfo();
+
     setIsLoading(false);
-   
-  }, [fetchData]); //Förstår inte varför ESLint vill ha API_URL här är det för hur jag använt js i strängen?
+  }, [fetchData, API_URL]); //Förstår inte varför ESLint vill ha API_URL här är det för hur jag använt js i strängen?
+
+  const changeHowMany = (e) => {
+    setHowMany(e.target.value);
+  };
+  console.log(`Will load ${howMany} pokemons`);
 
   return (
     <div>
       <h1>Pokemon Loader</h1>
       <input value={howMany} onChange={changeHowMany} />
-      <button onClick={()=> setFetchData(!fetchData)}>Hämta Pokemons</button>
+      <button onClick={() => setFetchData(!fetchData)}>Hämta Pokemons</button>
       <ProgressBar isLoading={isLoading} />
 
       <h5>Antal Pokémon: {pokemons}</h5>
-      {pokemonName.map((poke) => poke.name)}
+      <h6>Heres a list of them</h6>
+      <div className="pokemon-holder">
+        {pokemonData.map((poke) => (
+          <div>
+            <p>{poke.name}</p>
+            
+            <img src={poke.sprites.front_default} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
