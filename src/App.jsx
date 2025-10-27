d2d080f7c1b78892e549a3b280d2dd2a4e5dc48a
moshito <mosho@me.com>
2025-10-27 17:24:34 +0100

    Progress so far, to show Ahmad

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
 import { useEffect, useState } from "react";
 import ProgressBar from "./ProgressBar";
-
+console.log("Powered by: Mos\n===============")
 function App() {
-  
+  const [howMany, setHowMany] = useState(7);
   const API_URL = `https://pokeapi.co/api/v2/pokemon?limit=${howMany}&offset=0`;
-  const [howMany, setHowMany] = useState("")
-  const [pokemons, setPokemons] = useState(null);
+  const [pokemons, setPokemons] = useState(0);
+  const [pokemonName, setPokemonName] = useState([])
   const [isLoading, setIsLoading] = useState(false);
-  const [btnClicked, setBtnClicked] = useState(false);
+  const [fetchData, setFetchData] = useState(false);
 
-  const changeHowMany = () => {
-    setHowMany()
-  }
+  const changeHowMany = (e) => {
+    setHowMany(e.target.value);
+  };
+  console.log(`Will load ${howMany} pokemons`)
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
 
     async function getPokemons() {
       console.log("Retiving pokemons");
       const getTheData = await fetch(API_URL);
       const parsedData = await getTheData.json();
-
-      setPokemons(parsedData);
+      setPokemons(parsedData.results.length);
+      setPokemonName(parsedData.results)
+      console.log(typeof pokemonName)
     }
     getPokemons();
-
-    return;
-  }, []);
+    setIsLoading(false);
+   
+  }, [fetchData]); //Förstår inte varför ESLint vill ha API_URL här är det för hur jag använt js i strängen?
 
   return (
     <div>
       <h1>Pokemon Loader</h1>
       <input value={howMany} onChange={changeHowMany} />
-      <button onClick={}>Hämta Pokemons</button>
+      <button onClick={()=> setFetchData(!fetchData)}>Hämta Pokemons</button>
       <ProgressBar isLoading={isLoading} />
 
-      <p>Antal Pokémon: {pokemons}</p>
+      <h5>Antal Pokémon: {pokemons}</h5>
+      {pokemonName.map((poke) => poke.name)}
     </div>
   );
 }
